142eb958fc154bb730a473aac631be271ca2b7e9
Adam Hinz <hinz.adam@gmail.com>
2015-02-28 19:08:13 -0500

    Add ring server

--- src/wellframe/core.clj
+++ src/wellframe/core.clj
@@ -1,7 +1,37 @@
-(ns wellframe.core)
+(ns wellframe.core
+  (:require [compojure.core :refer :all]
+            [compojure.route :as route]
+            [wellframe.bayes :as bayes]
+            [clojure.java.io :as io]))
 
+(set! *warn-on-reflection* true)
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn- slurp-dir [path n]
+  (->> path
+       io/file
+       .listFiles
+       (take n)
+       (map slurp)))
+
+(defn- create-initial-state [n]
+  (let [spam-messages (map vector (slurp-dir "/Users/ahinz/Downloads/spam_2" n) (repeat true))
+        ham-messages (map vector (slurp-dir "/Users/ahinz/Downloads/easy_ham" n) (repeat false))
+        messages (concat spam-messages ham-messages)]
+    (bayes/build-model messages)))
+
+(def state (atom (create-initial-state 100)))
+
+(defn- train [body is-spam]
+  (swap! state bayes/additive-merge-states (bayes/build-model [[body is-spam]])))
+
+(defroutes app
+  (GET "/model" []
+       (println (count @state))
+       (str @state))
+  (POST "/train/spam" {body :body}
+        (train (slurp body) true))
+  (POST "/train/not-spam" {body :body}
+        (train (slurp body) false))
+  (POST "/classify" {body :body}
+        (str (bayes/pr-message-is-spam @state (slurp body))))
+  (route/not-found "<h1>Page not found</h1>"))
